a578a9de79b3b040918b13b27098c3796a5aebb6
Mikhail V. Kuzmich <kmv85@mail.ru>
2022-12-09 19:55:00 +0300

    change directories

--- application/src/main/java/com/kuzmich/searchengineapp/repository/FieldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kuzmich.searchengineapp.repository;
-
-import com.kuzmich.searchengineapp.entity.Field;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FieldRepository extends JpaRepository<Field, Integer> {
-}
